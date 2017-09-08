ab79188b856320392488a75662c5a5d6b04ddb66
kimpossible6 <thestars.thesnowandthefire@gmail.com>
2017-09-08 02:06:41 -0400

    update
    update person4

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -31,7 +31,13 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
+      String a = "";
+      for (int i = 0; i < input.length(); i++) {
+        char c = input.charAt(i);
+        c = c+1; 
+        a +=c;
+      }
+      return a;
     }
     
     /**
