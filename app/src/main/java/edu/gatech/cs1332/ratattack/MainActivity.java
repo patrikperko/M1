e674b68b42c2a41dd3bd26aebd2eaab67c471a9a
Peterson, Brent Carswell <bpeterson41@gatech.edu>
2017-09-21 21:06:24 -0400

    Rat-Attack 17,19 - Create opening screen, display
    * Make login screen
    * Username and password fields
    * Hide password

--- app/src/main/java/edu/gatech/cs1332/ratattack/MainActivity.java
+++ app/src/main/java/edu/gatech/cs1332/ratattack/MainActivity.java
@@ -2,12 +2,32 @@ package edu.gatech.cs1332.ratattack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        signUpButton = (Button) findViewById(R.id.signUpButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginScreen();
+            }
+        });
     }
+
+    public void loginScreen(){
+        setContentView(R.layout.activity_login);
+    }
+
 }
