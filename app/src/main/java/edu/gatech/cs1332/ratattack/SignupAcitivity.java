f696157f62376055662d190d6580e2817810bbf6
kimpossible6 <thestars.thesnowandthefire@gmail.com>
2017-09-23 22:55:13 -0400

    Rat-Attack Issue202123
    *Set up the registration page
    *login checking: default login: username: user
    *login checking: default login: password: pass
    *please use the username and password above to test the application
    *Set up the login successful page: when login successfully
    *The page will go back to the homepage after press "logout"
    *Ziwei Miao

---
+++ app/src/main/java/edu/gatech/cs1332/ratattack/SignupAcitivity.java
@@ -0,0 +1,13 @@
+package edu.gatech.cs1332.ratattack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignupAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_acitivity);
+    }
+}
