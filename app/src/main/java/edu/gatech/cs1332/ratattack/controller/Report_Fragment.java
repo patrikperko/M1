a23b918fbe59f0487e502e3837e3f2bb176fbc6c
Peterson, Brent Carswell <bpeterson41@gatech.edu>
2017-10-20 15:11:22 -0400

    45 - Added way to submit new rat

--- app/src/main/java/edu/gatech/cs1332/ratattack/controller/Report_Fragment.java
+++ app/src/main/java/edu/gatech/cs1332/ratattack/controller/Report_Fragment.java
@@ -1,19 +1,33 @@
 package edu.gatech.cs1332.ratattack.controller;
 
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 import edu.gatech.cs1332.ratattack.R;
+import edu.gatech.cs1332.ratattack.model.Rat;
 
 public class Report_Fragment extends Fragment {
     public static Report_Fragment newInstance() {
         Report_Fragment fragment = new Report_Fragment();
         return fragment;
     }
+
+    private EditText locationText;
+    private EditText zipText;
+    private EditText addressText;
+    private EditText cityText;
+    private EditText boroughText;
+    private EditText keyText;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
@@ -22,6 +36,28 @@ public class Report_Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_report__fragment, container, false);
+        View rootView = inflater.inflate(R.layout.activity_report__fragment, container, false);
+        locationText = (EditText) rootView.findViewById(R.id.locationType);
+        zipText = (EditText) rootView.findViewById(R.id.zipCode);
+        addressText = (EditText) rootView.findViewById(R.id.address);
+        cityText = (EditText) rootView.findViewById(R.id.city);
+        boroughText = (EditText) rootView.findViewById(R.id.borough);
+        keyText = (EditText) rootView.findViewById(R.id.key);
+        final Button report = (Button) rootView.findViewById(R.id.report);
+        report.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (locationText.getText().toString().trim().length() > 0 && zipText.getText().toString().trim().length() > 0 &&
+                        cityText.getText().toString().trim().length() > 0 && addressText.getText().toString().trim().length() > 0 &&
+                        boroughText.getText().toString().trim().length() > 0 && keyText.getText().toString().trim().length() > 0) {
+                    Rat newRat = new Rat(keyText.getText().toString(), DateFormat.getDateInstance().format(new Date()), locationText.getText()
+                            .toString(), zipText.getText().toString(), addressText.getText().toString(), cityText.getText().toString(),
+                            boroughText.getText().toString(), null, null);
+                }
+            }
+        });
+        return rootView;
     }
 }
+
