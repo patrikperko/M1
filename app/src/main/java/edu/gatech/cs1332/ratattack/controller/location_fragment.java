83ae00c3d850f41d049453723cce2b48d6397220
kimpossible6 <thestars.thesnowandthefire@gmail.com>
2017-10-21 23:58:56 -0400

    Google Map
    #Show google map in location fragment

--- app/src/main/java/edu/gatech/cs1332/ratattack/controller/location_fragment.java
+++ app/src/main/java/edu/gatech/cs1332/ratattack/controller/location_fragment.java
@@ -2,6 +2,7 @@ package edu.gatech.cs1332.ratattack.controller;
 
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,6 +13,15 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,12 +29,16 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import edu.gatech.cs1332.ratattack.R;
 import edu.gatech.cs1332.ratattack.model.Database;
 import edu.gatech.cs1332.ratattack.model.Rat;
 
-public class location_fragment extends Fragment {
+public class location_fragment extends Fragment implements OnMapReadyCallback {
+    GoogleMap mGoogleMap;
+    MapView mMapView;
+    View mView;
     Button ratDataButton;
     public static location_fragment newInstance() {
         location_fragment fragment = new location_fragment();
@@ -38,6 +52,29 @@ public class location_fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_location_fragment, container, false);
+        mView = inflater.inflate(R.layout.activity_location_fragment, container, false);
+        return mView;
     }
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mMapView = (MapView) mView.findViewById(R.id.map);
+        if(mMapView != null) {
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        mGoogleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(40.7484,-73.9857)).title("lol").snippet("I found a rat!"));
+        CameraPosition rat = CameraPosition.builder().target(new LatLng(40.7484,-73.9857)).zoom(16).bearing(0).tilt(45).build();
+        googleMap.moveCamera((CameraUpdateFactory.newCameraPosition(rat)));
+
+    }
+
 }
