40695b4188c982caa9d97245398fc5c549d4b57e
patrikperko <patrikpperko@gmail.com>
2017-11-01 22:23:24 -0400

    RA 65: Prompt user for graph setup info
    * Add DatePicker to Graph_Fragment

--- app/src/main/java/edu/gatech/cs1332/ratattack/controller/Graph_Fragment.java
+++ app/src/main/java/edu/gatech/cs1332/ratattack/controller/Graph_Fragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import edu.gatech.cs1332.ratattack.R;
 
@@ -13,6 +14,12 @@ import edu.gatech.cs1332.ratattack.R;
  */
 
 public class Graph_Fragment extends Fragment {
+    private static final String DATE = "date";
+
+    View graphView;
+    String fromdate;
+    String todate;
+
     public static Graph_Fragment newInstance() {
         Graph_Fragment fragment = new Graph_Fragment();
         return fragment;
@@ -26,10 +33,36 @@ public class Graph_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(
+        graphView = inflater.inflate(
                 R.layout.activity_graph_fragment,
                 container,
                 false);
-        return rootView;
+        return graphView;
+    }
+
+    public void onViewCreated(View view,
+                              Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final EditText startdate = (EditText)graphView.findViewById(R.id.startdate);
+        startdate.setKeyListener(null);
+        startdate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Datepicker dialog = new Datepicker(v);
+                fromdate = dialog.gettxtdate();
+                dialog.show(getActivity().getFragmentManager(),DATE);
+            }
+        });
+        final EditText enddate = (EditText)graphView.findViewById(R.id.enddate);
+        enddate.setKeyListener(null);
+        enddate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Datepicker dialog = new Datepicker(v);
+                todate = dialog.gettxtdate();
+                dialog.show(getActivity().getFragmentManager(),DATE);
+            }
+        });
     }
 }
