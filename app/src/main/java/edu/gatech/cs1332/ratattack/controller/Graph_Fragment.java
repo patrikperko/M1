bb16eb91a7c09f531238206650d96cbd6f4d3338
patrikperko <patrikpperko@gmail.com>
2017-11-01 23:48:16 -0400

    RA 69: Button Changes Graph Data
    * Add listener to graphButton
    * Add method updateGraph() to update graph when button is pressed

--- app/src/main/java/edu/gatech/cs1332/ratattack/controller/Graph_Fragment.java
+++ app/src/main/java/edu/gatech/cs1332/ratattack/controller/Graph_Fragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import edu.gatech.cs1332.ratattack.R;
@@ -16,7 +17,7 @@ import com.jjoe64.graphview.series.LineGraphSeries;
  * Created by Patrik on 11/1/2017.
  */
 
-public class Graph_Fragment extends Fragment {
+public class Graph_Fragment extends Fragment implements View.OnClickListener {
     private static final String DATE = "date";
 
     View graphView;
@@ -68,12 +69,28 @@ public class Graph_Fragment extends Fragment {
             }
         });
 
+        updateGraph(0,0);
+        Button graphButton = (Button) graphView.findViewById(R.id.graphButton);
+        graphButton.setOnClickListener(this);
+
+    }
+
+    public void onClick(View graphView) {
+        switch (graphView.getId()) {
+            case R.id.graphButton:
+                updateGraph(3, 3);
+                break;
+        }
+    }
+
+    private void updateGraph(int x, int y) {
         GraphView graph = (GraphView) graphView.findViewById(R.id.graph);
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3)
+                new DataPoint(x, y),
+                new DataPoint(x + 1, y + 1),
+                new DataPoint(x + 2, y + 2)
         });
+
         graph.addSeries(series);
     }
 }
